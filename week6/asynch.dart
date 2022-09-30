570d363475a026ce7f5bd3ab2245a87518696bab
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-09-30 11:59:37 +0300

    finish

--- week6/asynch.dart
+++ week6/asynch.dart
@@ -1,5 +1,12 @@
 Future<void> main(List<String> args) async {
   print('a');
+
+  Stream<int> bankMoney = Stream.empty();
+  bankMoney = dataAddBankMoney(55, 3);
+  // bankMoney.listen((event) {
+  //   print(event);
+  // });
+  print(await bankMoney.where((event) => event == 258).toList());
   // await  Future.forEach([1,2,3,4,5,6],(int element) async {
   //   await Future.delayed(Duration(seconds: 2));
   //   print('islem bitti $element');
@@ -7,31 +14,29 @@ Future<void> main(List<String> args) async {
   // await Future.delayed(Duration(seconds: 2));
   print('ab');
 
-  //5 sanit-ye bekle sonra gel hicbir musteri alma
+  //5 sanitye bekle sonra gel hicbir musteri alma
 
-  // print('hello');
+  print('hello');
 
-  // Future.delayed(Duration(seconds: 3)).whenComplete(() {
-  //   print('hello2');
-  // });
-  // print('hello3');
+  Future.delayed(Duration(seconds: 1)).whenComplete(() {
+    print('hello2');
+  });
+  print('hello3');
 
-  // Future.delayed(Duration(seconds: 2)).whenComplete(() {
-  // print('hello4');
-
-  // });
-  Stream<int> bankMoney = Stream.empty();
+  Future.delayed(Duration(seconds: 2)).whenComplete(() {
+    print(
+        'hello4'); //bunun saniyesi en büyük olduğu için bu işlemi en son yapıyor
+  });
 }
 
-Stream<int> dataAddBankMoney(int retryCount, int money) {
-  int _retryCount=0;
+Stream<int> dataAddBankMoney(int retryCount, int money) async* {
+  int _retryCount = 0;
+  await Future.delayed(Duration(seconds: 1));
 
   while (_retryCount < retryCount) {
-
-
     _retryCount++;
 
-    yield money+=5;
+    yield money += 5;
   }
 }
 
