27b4b95ee1e6b3aee51b2d0de5c34c35d4b4fe06
Cevdet Eren Özbozkurt <cevdet.eren.2001@gmail.com>
2022-09-30 17:16:48 +0300

    Bittiiii!!!

--- week6/generic.dart
+++ week6/generic.dart
@@ -1,10 +1,26 @@
 void main(List<String> args) {
   final user = User('rtr');
 
-  print(user.data);//type değişklik gösterebilir.
+  print(user.data); //type değişklik gösterebilir.
+
+  final adminUser = AdminUser();
+  removeUserAll(adminUser);//sadec admin useden türeyenler oluşturulabilir.
 }
 
 class User<T> {
   T data;
   User(this.data);
+}
+
+void removeUserAll<T extends IAdmin>(T data) {
+  data.removeUser();
+}
+
+abstract class IAdmin {
+  void removeUser() {}
+}
+
+class AdminUser extends IAdmin {
+  @override
+  void removeUser() {}
 }
